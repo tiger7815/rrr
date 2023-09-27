dd1b5777af581506bd69d1772768b21beb846e2a
LPRPDV <144261678+LPRPDV@users.noreply.github.com>
2023-09-27 22:21:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ import os
 import io
 from get_video_info import get_video_attributes, get_video_thumb
 
-API_ID = 9541960
-API_HASH = "17a1aa509b35564481dae98d89d0191d"
-BOT_TOKEN = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
-AUTH_USERS = 942518345
-sudo_users = [942518345]
+API_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+AUTH_USERS = 
+sudo_users = []
 bot = Client(
     "bot",
     bot_token=BOT_TOKEN,
