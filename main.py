4776ecd0700e010fbca9cd0a84cd86b1ba4428b0
LPRPDV <144261678+LPRPDV@users.noreply.github.com>
2023-10-02 12:00:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ async def account_login(bot: Client, m: Message):
         for i in range(arg, len(links)):
 
             url = links[i][1]
-            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/","").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*","").replace("download",".pdf").replace(".","").strip()LPRPDV 
+            name1 = links[i][0].replace("\t", "").replace(":", "").replace("/","").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*","").replace("download",".pdf").replace(".","").strip()
             if ".pdf" in url or "pdf" in name1:
                 name = f"{str(count).zfill(3)}) {name1.replace('pdf', '')} LPRPDV.pdf"
                 r = requests.get(url, allow_redirects=True)
